356ad6cc4e6a86a8ba3fe88df9cb7c57368da19d
ethonion <57050635+ethonion@users.noreply.github.com>
2024-06-25 23:46:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-var wbhk = 'https://discord.com/api/webhooks/1255379818136997940/0lyZvuipRxr-_zhZ8SGw56HwQPAO48Fg0U5MhFYz4ZcF5WvBRpSFC3gMm26Ks3jkCiK5'
+var wbhk = 'http://spain2.firecloudllc.info:36568'
